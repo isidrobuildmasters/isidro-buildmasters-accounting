f6b00c4d7f570eea385348c6f3aa3ba36e1c878a
Isidro Buildmasters <mitchmontesa@gmail.com>
2026-08-17 01:36:43 +0800

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "Isidro Buildmasters Accounting",
+  description: "Cloud Construction Accounting & Project Management",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
